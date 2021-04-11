be9a05448f898553a123ed3783d17063a230e2ae
dmitry.khlebnikov <james-bob@mail.ru>
2021-04-11 15:38:50 +0300

    version 2

--- shift_array/Program.cs
+++ shift_array/Program.cs
@@ -5,120 +5,56 @@ namespace shift_array
     class Program
     {
         static void Main(string[] args)
-        {/*
-            int maxRange = 200;
-            for (int i = 1; i < maxRange; i++)
-            {
-                for (int j = -i; j <= i; j++)
-                {
-                    Console.WriteLine($"Array lenght {i} Array shift {j}");
-                    shiftArray(i, j);
-                }
-            }*/
+        {
             Console.WriteLine("input lenght");
-            int lenght = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
             Console.WriteLine("input shift");
             int shift = int.Parse(Console.ReadLine());
-            shiftArray(lenght, shift);/**/
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = i;
+            }
+            shiftArray(ref arr, shift);
         }
 
-        static void shiftArray(int ArrayLenght, int shift)
+        static void shiftArray(ref int[] arr, int shift)
         {
+            if (arr.Length==0)
+            {
+                return;//can't shift
+            }
             int ShiftCount = 0;
-            int halfArray = ArrayLenght / 2;
-            int calculatedShift;//if Abs(shift) > ArrayLenght/2 we can perform a shift in the opposite direction, by a smaller amoun
-            if (Math.Abs(shift % ArrayLenght) > halfArray)
+            int calculatedShift = shift % arr.Length;
+            if (calculatedShift < 0)
             {
-                if (shift > 0)
+                calculatedShift = arr.Length + calculatedShift;
+            }
+            if (calculatedShift == 0)
+            {
+                return;//not need shift
+            }
+            int tail = 0;
+            int head = calculatedShift;
+            int topush= arr[tail];
+            while (ShiftCount < arr.Length - 1)
+            {
+                if (head == tail)
                 {
-                    calculatedShift = shift % ArrayLenght - ArrayLenght;
+                    arr[tail++] = topush;
+                    topush = arr[tail];
+                    head = (tail + calculatedShift) % arr.Length; 
                 }
                 else
                 {
-                    calculatedShift = shift % ArrayLenght + ArrayLenght;
+                    int tmp = arr[head];
+                    arr[head] = topush;
+                    topush = tmp;
+                    head = (head + calculatedShift) % arr.Length;
                 }
+                ShiftCount++;
             }
-            else
-            {
-                if (Math.Abs(shift) >= ArrayLenght)
-                {
-                    calculatedShift = shift % ArrayLenght;
-                }
-                else
-                {
-                    calculatedShift = shift;
-                }
-            }
-            int tail;
-            int head;
-            if (calculatedShift > 0)
-            {
-                tail = 0;
-                head = calculatedShift;
-            }
-            else
-            {
-                tail = ArrayLenght - 1;
-                head = ArrayLenght - 1 + calculatedShift;
-            }
-            int[] arr = new int[ArrayLenght];
-            for (int i = 0; i < ArrayLenght; i++)
-            {
-                arr[i] = i+1;
-            }
-            arr[head] = arr[tail];
-            int topush = head;
-            if (!(calculatedShift == 0))
-            {
-                while (ShiftCount < ArrayLenght - 1)
-                {
-                    if (head == tail)
-                    {
-                        if (calculatedShift > 0)
-                        {
-                            head++;
-                        }
-                        else
-                        {
-                            head--;
-                        }
-                        arr[tail] = tmp;
-                        tmp = arr[head];
-                        tail = head;
-                        if (ShiftCount >= ArrayLenght - 1)
-                        {
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        arr[head] = ++ShiftCount;//
-                    }
-                    head = (head + calculatedShift) % ArrayLenght;
-                    if (head < 0)
-                    {
-                        head += ArrayLenght;
-                    }
-                }
-                arr[head] = tmp;
-                Console.WriteLine("Shift success");
-                if (true)
-                {
-                    for (int i = 0; i < arr.Length; i++)
-                    {
-                        if (arr[i] == i)
-                        {
-                            throw new Exception();
-                        }
-                        Console.Write(arr[i] + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Can't shift");
-            }
+            arr[head] = topush;//shift complete
         }
     }
 }
